a23c599e48f19cd3a865a255a718b177f592485a
Islam <islamabdoulradi@gmail.com>
2023-07-27 05:44:34 +0300

    shell word counter file

---
+++ shell_Wc.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * allWc - it's act like wc in linux
+ * @str: String to be parsed.
+ * @separators: One or more delimiters.
+ * @arr: Pointer to array where the word lengths will be stored.
+ *
+ * Return: Number of words.
+ */
+
+int allWc(char *str, char *separators, unsigned int *arr)
+{
+	if (!str)
+	{
+		return (0);
+	}
+
+	int word_count = 0;
+	bool inside_word = false;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		if (strchr(separators, *p) != NULL)
+		{
+			inside_word = false;
+		}
+		else if (!inside_word)
+		{
+			inside_word = true;
+			arr[word_count++] = 1;
+		}
+		else
+		{
+			arr[word_count - 1]++;
+		}
+		p++;
+	}
+
+	return (word_count);
+}
